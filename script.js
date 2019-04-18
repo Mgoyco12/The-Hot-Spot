30baa75af2c2b977bc5835a10682ab028b5facf3
Mgoyco12 <43241864+Mgoyco12@users.noreply.github.com>
2019-04-18 09:13:40 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+var imgUrl = [];
+var item = [];
+var price = [];
+$(".add").click(function(){
+    var num = parseInt($(".in3").val()); 
+    var img = $(".in1").val();
+    var name = $(".in2").val();
+    });
