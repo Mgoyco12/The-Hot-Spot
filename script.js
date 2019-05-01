cb134d25a7600299f050bd00128b41914862f69b
Mgoyco12 <43241864+Mgoyco12@users.noreply.github.com>
2019-05-01 09:16:07 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ $(".add").click(function(){
     var num = parseInt($(".in3").val()); 
     var img = $(".in1").val();
     var name = $(".in2").val();
+    imgUrl.push(img);
+    item.push(name);
+    price.push(num);
     });
